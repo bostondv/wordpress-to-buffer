1e706a19210b487bf364293fbeb2e3634b96cd89
Boston Dell-Vandenberg <boston@pomelodesign.com>
2013-01-04 16:03:45 -0500

    update file references

--- admin.php
+++ admin.php
@@ -3,40 +3,40 @@
 	$token = get_option('ft_bufferapp_token');
 	$profiles = get_option('ft_bufferapp_profiles');
 	$selected = get_option('ft_bufferapp_profiles_selected');
-	
+
 	if (isset($_POST['submit'])) {
 		if (function_exists('current_user_can') && !current_user_can('manage_options')) {
 			die(__('Cheatin&#8217; uh?'));
 		}
-		
+
 		check_admin_referer('ft-bufferapp-config');
 		$key = $_POST['ft_bufferapp_consumer_key'];
 		$token = $_POST['ft_bufferapp_token'];
-		
+
 		if($key && $token) {
 			update_option('ft_bufferapp_consumer_key', $key);
 			update_option('ft_bufferapp_token', $token);
-			
+
 			ft_bufferapp_syncprofiles();
 			$profiles = get_option('ft_bufferapp_profiles');
 			$selected = get_option('ft_bufferapp_profiles_selected');
 		}
 	} elseif(isset($_POST['update'])) {
 		$selected = array();
-		
+
 		if(isset($_POST['profiles'])) {
 			foreach($_POST['profiles'] as $profile) {
 				$selected[] = $profile;
 			}
 		}
-		
+
 		update_option('ft_bufferapp_profiles_selected', $selected);
 	} elseif (isset($_POST['clear'])) {
 		delete_option('ft_bufferapp_consumer_key');
 		delete_option('ft_bufferapp_token');
 		delete_option('ft_bufferapp_profiles');
 		delete_option('ft_bufferapp_profiles_selected');
-		
+
 		$key = null;
 		$token = null;
 		$profiles = none;
@@ -51,19 +51,19 @@
 		$profiles = get_option('ft_bufferapp_profiles');
 		$selected = get_option('ft_bufferapp_profiles_selected');
 	}
-	
+
 	if(!is_array($selected) && $profiles && is_array($profiles)) {
 		$selected = array();
 		foreach($profiles as $profile) {
 			$selected[] = $profile['id'];
 		}
 	}
-	
+
 	?>
 	<div class="wrap">
 		<div id="icon-options-general" class="icon32"><br></div>
 		<h2><?php printf(__('%s settings', 'ft-bufferapp'), 'Buffer'); ?></h2>
-		
+
 		<?php if(!$key || !$token || !$profiles) { ?>
 			<div class="narrow">
 				<h3><?php print(__('First, create an app that your blog can use', 'ft-bufferapp')); ?></h3>
@@ -75,7 +75,7 @@
 						<ul>
 							<li><?php printf(
 								__('<strong>Important:</strong> Set the callback URL to <code>%s</code>', 'ft-bufferapp'),
-								plugins_url('post-to-buffer/callback.php')
+								plugins_url('wordpress-to-buffer/callback.php')
 							); ?></li>
 						</ul>
 					</li>
@@ -83,7 +83,7 @@
 						__('Put the Client ID and Token in the boxes below', 'ft-bufferapp')
 					); ?></li>
 				</ol>
-				
+
 				<form method="post">
 					<div>
 						<h3><?php print(__('App details', 'ft-bufferapp')); ?></h3>
@@ -93,17 +93,17 @@
 									<td><label for="ft_bufferapp_consumer_key">Client ID</label></td>
 									<td><input id="ft_bufferapp_consumer_key" name="ft_bufferapp_consumer_key" type="text" value="<?php echo $key; ?>" /></td>
 								</tr>
-								
+
 								<tr>
 									<td><label for="ft_bufferapp_consumer_url">Redirect URL</label></td>
-									<td><input id="ft_bufferapp_consumer_url" type="text" readonly value="<?php echo plugins_url('post-to-buffer/callback.php'); ?>" /></td>
+									<td><input id="ft_bufferapp_consumer_url" type="text" readonly value="<?php echo plugins_url('wordpress-to-buffer/callback.php'); ?>" /></td>
 								</tr>
-								
+
 								<tr>
 									<td><label for="ft_bufferapp_token">Access token</label></td>
 									<td><input id="ft_bufferapp_token" name="ft_bufferapp_token" type="text" value="<?php echo $token; ?>" /><br /></td>
 								</tr>
-								
+
 								<tr>
 									<td></td>
 									<td>
@@ -112,7 +112,7 @@
 								</tr>
 							</tbody>
 						</table>
-						
+
 						<?php wp_nonce_field('ft-bufferapp-config'); ?>
 						<p class="submit">
 							<input type="submit" name="submit" value="<?php _e('Update options &raquo;', 'ft-bufferapp'); ?>" />
@@ -124,13 +124,13 @@
 				</form>
 			</div>
 		<?php } ?>
-		
+
 		<?php if($key && $token && $profiles) { ?>
 			<h3><?php printf(__('Profiles', 'ft-bufferapp')); ?></h3>
 			<p>
 				<?php printf(__('We\'ve found the following profiles in your %s account.<br />When you create a post, you\'ll be able to choose which services to post to.', 'ft-bufferapp'), 'Buffer'); ?>
 			</p>
-			
+
 			<form method="post">
 				<ul>
 					<?php foreach($profiles as $profile) { ?>
@@ -142,14 +142,14 @@
 						</li>
 					<?php } ?>
 				</ul>
-				
+
 				<p>
 					<?php printf(
 						__('If you\'ve made any changes, you can synchronise this blog with your %s account again.', 'ft-bufferapp'),
 						'Buffer'
 					); ?>
 				</p>
-				
+
 				<p class="submit">
 					<input type="submit" name="update" class="button-primary" value="<?php _e('Save Changes', 'bt-bufferapp'); ?>" />
 					<input type="submit" name="sync" value="<?php _e('Sync My Profiles &raquo;', 'bt-bufferapp'); ?>" />
--- callback.php
+++ callback.php
@@ -5,16 +5,16 @@ require_once(dirname(__FILE__) . '/../../../wp-blog-header.php');
 if(isset($_GET['state']) && isset($_GET['code'])) {
 	$key = get_option('ft_bufferapp_consumer_key');
 	$secret = get_option('ft_bufferapp_consumer_secret');
-	
+
 	$url = 'https://api.bufferapp.com/1/oauth2/token.json';
 	$fields = array(
 		'client_id' => $key,
 		'client_secret' => $secret,
-		'redirect_uri' => plugins_url('post-to-buffer/callback.php'),
+		'redirect_uri' => plugins_url('wordpress-to-buffer/callback.php'),
 		'code' => $_GET['code'],
 		'grant_type' => 'authorization_code'
 	);
-	
+
 	$r = wp_remote_post($url, array('body' => $fields));
 	if(!function_exists('json_decode')) {
 		wp_die('A JSON library does not appear to be installed.\n\nPlease contact your server admini f you need help installing one.');
